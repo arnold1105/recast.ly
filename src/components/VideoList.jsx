7a9bd126a94ad596fcb1021981f30c70dfea406a
arnold1105 <arnold.b.cheung@gmail.com>
2019-01-16 20:13:19 -0800

    Implement through VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,14 +1,16 @@
 // import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js';
 
-var VideoList = (props) => (
+  var VideoList = (props) => (
+       
   <div className="video-list">
     {/*<div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>*/}
-    {props.videos.map((eachVideo) =>
-      <videoListEntry videos={eachVideo}/>
+    {props.videos.map((eachVideo) => 
+      <VideoListEntry video={eachVideo}/>
     )}
   </div>
 );
