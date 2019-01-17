769fbabf1fb31e0cdafdadb698c551ee53f5b094
arnold1105 <arnold.b.cheung@gmail.com>
2019-01-16 20:46:55 -0800

    Will implement Video Player

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 // import exampleVideoData from '../data/exampleVideoData.js';
 import VideoListEntry from './VideoListEntry.js';
 
-  var VideoList = (props) => (
+var VideoList = (props) => (
        
   <div className="video-list">
     {/*<div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -18,7 +18,7 @@ import VideoListEntry from './VideoListEntry.js';
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: React.PropTypes.array.isRequired
+  video: React.PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
